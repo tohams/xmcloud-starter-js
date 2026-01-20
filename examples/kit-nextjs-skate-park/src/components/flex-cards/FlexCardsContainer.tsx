835e83edcbbe018697ef2d65d7a306e0c409c040
Thomas Lin <tohams@gmail.com>
2026-01-20 15:42:18 -0600

    Center flex cards group with inline grid
    - Use inline-grid so grid width shrinks to cards
    
    - Keeps overall group centered via flex wrapper

--- examples/kit-nextjs-skate-park/src/components/flex-cards/FlexCardsContainer.tsx
+++ examples/kit-nextjs-skate-park/src/components/flex-cards/FlexCardsContainer.tsx
@@ -121,7 +121,7 @@ const Default = (props: FlexCardsContainerProps): JSX.Element => {
       <div className="component-content py-16 mt-16">
         <div className="container mx-auto px-4 max-w-6xl" style={{ marginLeft: 'auto', marginRight: 'auto' }}>
           <div className="flex justify-center">
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 justify-items-center">
+            <div className="inline-grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 justify-items-center">
               {cards.map((card, index) => {
               // In editing mode, show all cards. In preview/live, only show cards with content
               if (!isEditing && !hasCardContent(card)) {
